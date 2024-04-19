0a519957b987ca055946459123876298bd94f2a1
SebSanUwU <juancamargo206@gmail.com>
2024-04-19 12:22:51 -0500

    Eventos de amigos y salas

--- socket.js
+++ socket.js
@@ -1,5 +1,8 @@
+const { emit } = require('process');
+
 const MAX_PLAYERS_PER_ROOM = 5;
 const rooms = {};
+const playersConect = {};
 
 const app = require('express')();
 const http = require('http').createServer(app);
@@ -12,9 +15,9 @@ const io = require('socket.io')(http, {
 
 io.on('connection', (socket) => {
     let roomName;
-    socket.on('joinRoom', ( code ) => {
-         roomName = code;
-        
+    socket.on('joinRoom', (code) => {
+        roomName = code;
+
         // Verificar si la sala ya existe
         if (!rooms[roomName]) {
             // Crear la sala si no existe
@@ -47,39 +50,41 @@ io.on('connection', (socket) => {
         io.to(roomName).emit('updatePlayers', rooms[roomName].players.map(player => player.id));
     });
 
-   socket.on('updatePlayers', (data) => {
-    if (rooms[data.code] && rooms[data.code].players) { // Verificar si rooms[roomName] y rooms[roomName].players están definidos
-        const index = rooms[data.code].players.findIndex(player => player.id === socket.id);
-        if (index !== -1) {
-            rooms[data.code].players[index].posx = data.posx;
-            rooms[data.code].players[index].posy = data.posy;
-            rooms[data.code].players[index].velocityx = data.velocityx;
-            rooms[data.code].players[index].velocityy = data.velocityy;
-            rooms[data.code].players[index].animation = data.animation; 
-            rooms[data.code].players[index].key = data.key;
+    socket.on('updatePlayers', (data) => {
+        if (rooms[data.code] && rooms[data.code].players) { // Verificar si rooms[roomName] y rooms[roomName].players están definidos
+            const index = rooms[data.code].players.findIndex(player => player.id === socket.id);
+            if (index !== -1) {
+                rooms[data.code].players[index].posx = data.posx;
+                rooms[data.code].players[index].posy = data.posy;
+                rooms[data.code].players[index].velocityx = data.velocityx;
+                rooms[data.code].players[index].velocityy = data.velocityy;
+                rooms[data.code].players[index].animation = data.animation;
+                rooms[data.code].players[index].key = data.key;
 
+            }
+            io.to(data.code).emit('updatePlayers', rooms[data.code].players);
         }
-        io.to(data.code).emit('updatePlayers', rooms[data.code].players); 
-    }
-});
+    });
 
-socket.on('imHitted', () => {
-    for (const roomName in rooms) {
-        const index = rooms[roomName].players.findIndex(player => player.id === socket.id);
-        if (index !== -1) {
-            io.to(roomName).emit('imHitted', socket.id);
-            break;
+    socket.on('imHitted', () => {
+        for (const roomName in rooms) {
+            const index = rooms[roomName].players.findIndex(player => player.id === socket.id);
+            if (index !== -1) {
+                io.to(roomName).emit('imHitted', socket.id);
+                break;
+            }
         }
-    }
-});
+    });
 
-socket.on('updateSkeleton', (skeletonData) => {
-    io.to(skeletonData.code).emit('updateSkeleton', skeletonData);
-});
+    socket.on('updateSkeleton', (skeletonData) => {
+        io.to(skeletonData.code).emit('updateSkeleton', skeletonData);
+    });
 
-socket.on('goToDesert', (data) => {
-    io.to(roomName).emit('goToDesert', data); 
-});
+    socket.on('goToDesert', (data) => {
+        console.log(data.idOwner)
+        io.to(data.idOwner).emit('turnOffRoom', roomName)
+        io.to(roomName).emit('goToDesert', data);
+    });
 
     socket.on('disconnect', () => {
         for (const roomName in rooms) {
@@ -87,10 +92,39 @@ socket.on('goToDesert', (data) => {
             if (index !== -1) {
                 rooms[roomName].players.splice(index, 1);
                 io.to(roomName).emit('playerDisconnected', socket.id);
-                break; 
+                break;
             }
         }
     });
+    socket.on('sendFriendRequest', (data) => {
+        console.log(`El usuario con correo electrónico ${data.send} envio una solicitud.`);
+        if(playersConect[data.reciever]){
+            setTimeout(() => {
+                io.to(playersConect[data.reciever]).emit('friendRequestReceived', data.send);
+            }, 2000); 
+        }
+    });
+
+    socket.on('respondRequest',(data) => {
+        console.log(`El usuario con correo electrónico ${data.reciever} responde a solicitud de ${data.send} : ${data.respond}`);
+        if(playersConect[data.reciever]){
+            setTimeout(() => {
+                io.to(playersConect[data.send]).emit('friendRequestRespond', data.respond);
+            }, 2000); 
+        }
+    })
+    
+    socket.on('registPlayer',(data)=> {
+        // Verificar si el jugador esta registrado
+        if (!playersConect[data.user]) {
+            playersConect[data.user] = data.id;
+            console.log(`Se ha registrado el jugador ${data.user}`);
+        }else{
+            if(playersConect[data.user] != data.id){
+                playersConect[data.user] = data.id;
+            }
+        }
+    })
 });
 
 http.listen(2525, () => {
