01acdee9d8c48d207324b967aa1118943da398e8
Sebastian Camargo <92890055+SebSanUwU@users.noreply.github.com>
2024-04-24 15:07:13 -0500

    Update socket.js

--- socket.js
+++ socket.js
@@ -5,7 +5,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: {
-        origin: "http://localhost:4200",
+        origin: "https://cenizasdelpasadogame.azurewebsites.net",
         methods: ["GET", "POST"]
     }
 });
